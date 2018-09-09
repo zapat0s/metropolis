8a174998236797de562e726fb066158052e03906
Joshua Clark <zer0zapat0s@gmail.com>
2018-09-08 20:24:52 -0700

    Assets from LPC Base Assets

---
+++ core/assets/Grass and Water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Grass and Water" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="watergrass.png" width="96" height="192"/>
+</tileset>
