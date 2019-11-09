973613021f75577a3a2a575321415e41eda10613
Joshua Clark <zer0zapat0s@gmail.com>
2019-11-08 21:31:41 -0800

    Updated GFX application to lwjgl3.

--- desktop/src/main/java/digital/newshoes/metropolis/desktop/DesktopLauncher.java
+++ desktop/src/main/java/digital/newshoes/metropolis/desktop/DesktopLauncher.java
@@ -1,15 +1,17 @@
 package digital.newshoes.metropolis.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import digital.newshoes.metropolis.Metropolis;
+
+import static com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration.HdpiMode.Logical;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1920;
-        config.height = 1080;
-        config.useHDPI = true;
-        new LwjglApplication(new Metropolis(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Metropolis");
+        config.setWindowedMode(1920,1080);
+        config.setHdpiMode(Logical);
+        new Lwjgl3Application(new Metropolis(), config);
     }
 }
